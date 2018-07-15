e9e2c102c7865a2b8dafdeb9f9148326f7ad6167
geektao <348776286@qq.com>
2018-07-15 15:42:21 +0800

    m

--- src/main/java/org/springframework/samples/mvc/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/springframework/samples/mvc/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package org.springframework.samples.mvc.exceptions;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler
